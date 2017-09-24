e2c20cff24094cf6836423e028698f0ccbad656d
xsalefter <xsalefter@gmail.com>
2017-09-24 16:52:45 +0700

    add simple spring-boot application

---
+++ app/src/main/java/id/ac/telkomuniversity/impal/Application.java
@@ -0,0 +1,21 @@
+package id.ac.telkomuniversity.impal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
